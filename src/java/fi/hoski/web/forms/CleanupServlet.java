6a4471d4943e0c9c188ae94a47cb812caa6ef8b4
tkv <timo.vesalainen@iki.fi>
2013-05-14 14:02:32 +0300

    Fix

---
+++ src/java/fi/hoski/web/forms/CleanupServlet.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2013 Helsingfors Segelklubb ry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fi.hoski.web.forms;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Transaction;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Timo Vesalainen
+ */
+public class CleanupServlet extends HttpServlet {
+
+  /**
+   * Processes requests for both HTTP
+   * <code>GET</code> and
+   * <code>POST</code> methods.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    try {
+      out.println("<html>");
+      out.println("<head>");
+      out.println("<title>Servlet CleanupServlet</title>");
+      out.println("</head>");
+      out.println("<body>");
+      out.println("<h1>Servlet CleanupServlet at " + request.getContextPath() + "</h1>");
+      log("Starting cleanup");
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      Calendar cal = Calendar.getInstance();
+      long thisYear = cal.get(Calendar.YEAR);
+      Transaction tr = datastore.beginTransaction();
+      try {
+        Query yearQuery = new Query("Year");
+        PreparedQuery p1 = datastore.prepare(yearQuery);
+        for (Entity year : p1.asIterable()) {
+          Key key = year.getKey();
+          if (key.getId() < thisYear) {
+            Query query = new Query();
+            query.setAncestor(key);
+            query.setKeysOnly();
+            PreparedQuery p2 = datastore.prepare(query);
+            for (Entity e : p2.asIterable(FetchOptions.Builder.withChunkSize(500))) {
+              if (!"Year".equals(e.getKind()) && !"Attachment".equals(e.getKind())) {
+                log("removing " + e);
+                out.println("removing " + e+"<nbsp>");
+                datastore.delete(e.getKey());
+              }
+            }
+          }
+        }
+        tr.commit();
+        log("Cleanup ready");
+      } finally {
+        if (tr.isActive()) {
+          tr.rollback();
+          log("rollbacked");
+        }
+      }
+      out.println("</body>");
+      out.println("</html>");
+    } finally {
+      out.close();
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP
+   * <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP
+   * <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }// </editor-fold>
+}
